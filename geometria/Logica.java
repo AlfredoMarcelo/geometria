088c0661dbdf3b7bfc9db60055a5c99fe20f07dc
Alfredo Supanta <alfredo.supanta.26@gmail.com>
2022-04-11 23:11:51 -0400

    se implemento calculo de area rectangulo

--- geometria/Logica.java
+++ geometria/Logica.java
@@ -1,10 +1,55 @@
 package geometria;
 
+import java.util.Scanner;
+
 public class Logica {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//mostrar menu
+		//usar scanner para seleccion de opcion
+		//
+		int seleccion = 0;
+		do {
+		System.out.println("Seleccione una la operación que desea realizar:\n"
+					+ "1. Calcular área de un rectangulo\n"
+					+ "2. Calcular radio de un circulo\n"
+					+ "3. Calcular périmetro de un circulo\n"
+					+ "4. Calcular área de un circulo");
+			
+		System.out.printf("\nSeleccione una opción\n");
+		Scanner teclado = new Scanner(System.in);
+		seleccion = teclado.nextInt();
+			switch(seleccion) {
+			case 1:
+				System.out.println("selecciono opcion 1");
+				calcularAreaRectangulo();
+				seleccion = 0;
+				break;
+			case 2:
+				System.out.println("opcion 2");
+				seleccion = 0;
+				break;
+			default:
+				System.out.println("seleccione una opcion del menu");
+				seleccion = 0;
+				break;
+			}
+		}while(seleccion !=5);
 
 	}
+
+	private static void calcularAreaRectangulo() {
+		Rectangulo rectangulo = new Rectangulo();
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("\ningrese la base");
+		rectangulo.base = teclado.nextDouble();
+		System.out.println("ingrese la altura");
+		rectangulo.altura = teclado.nextDouble();
+		double resultado = rectangulo.calcularArea();
+		System.out.println("El area del rectangulo  es: => " + resultado + "\n");
+	}
+	
+	
+	
 
 }
